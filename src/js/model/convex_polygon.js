7117ca6859de7e38eca6f4f67963a2f5ba6c5d27
Astramast <azerqsdfazerqsdg@gmail.com>
2024-11-14 15:59:11 +0100

    Thu 14 Nov 15:59:11 CET 2024

--- src/js/model/convex_polygon.js
+++ src/js/model/convex_polygon.js
@@ -8,23 +8,40 @@ class ConvexPolygon {
 		}
 	}
 	getStartingFPVD(p, q, r) {
-		
+		perpendicular_bisectors = [perpendicularBisector(p, q), perpendicularBisector(q, r), perpendicularBisector(r, p)];
+		root = perpendicular_bisectors[0].intersect(perpendicular_bisectors[1]);
+		let points = [root];
+		let edges = [];
+		for (let i = 0; i < 3; i++) {
+			let a = perpendicular_bisectors[i].a;
+			let b = perpendicular_bisectors[i].b;
+			if (a.distance(input[i]) > a.distance(input[i + 2 % 3])) {
+				points.push(a);
+				edges.push((root, a));
+			}
+			else {
+				points.push(b);
+				edges.push((root, b));
+			}
+		}
+		return Tree([points, edges]);
 	}
 	//Farthest-Point Voronoi Diagram
 	getFPVD() {
-		if (this.points.length == 1) {
-			return this.points;
+		if (this.points.length <= 1) {
+			return Tree(this.points, null);
 		}
 		if (this.points.length == 2) {
-			return perpendicularBisector(this.points[0], this.points[1]);
+			return Tree(this.points, perpendicularBisector(this.points[0], this.points[1]));
 		}
 		if (this.points.length == 3) {
 			return getStartingFPVD(this.points[0], this.points[1], this.points[2]);
 		}
+		let tree = getStartingFPVD(this.points[0], this.points[1], this.points[2]);
 	}
 }
 
-function perpendicularBisector(p1, p2) {
+function perpendicularBisector(p, q) {
 	
 }
 
