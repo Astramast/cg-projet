2c7e1dcfdea3d3404eeae8a5a1da8544dfa7e3a7
Astramast <azerqsdfazerqsdg@gmail.com>
2024-11-25 08:23:11 +0100

    convex_polygon python -> js

--- src/js/model/convex_polygon.js
+++ src/js/model/convex_polygon.js
@@ -20,15 +20,15 @@ class ConvexPolygon {
 				break;
 			}
 		}
-		return VoronoiDiagram(c1.a, semilines);
+		return new VoronoiDiagram(c1.a, semilines);
 	}
 	//Farthest-Point Voronoi Diagram
 	getFPVD() {
 		if (this.points.length <= 1) {
-			return VoronoiDiagram(this.points, null);
+			return new VoronoiDiagram(this.points, null);
 		}
 		if (this.points.length == 2) {
-			return VoronoiDiagram(this.points, perpendicularBisector(this.points[0], this.points[1]));
+			return new VoronoiDiagram(this.points, perpendicularBisector(this.points[0], this.points[1]));
 		}
 		if (this.points.length === 3) {
 			return this.getStartingFPVD(this.points[0], this.points[1], this.points[2]);
@@ -45,10 +45,10 @@ class ConvexPolygon {
 		}
 		let fpvd = this.getStartingFPVD(copy[0], copy[1], copy[2]);
 		for (let i = 3; i < this.points.length; i++) {
-			p_i = this.points[i];
-			prevPi = vertices[p_i][0];
-			nextPi = vertices[p_i][1];
-			prevVCell = fpvd.getCellFromSite(prevPi);
+			let p_i = this.points[i];
+			let prevPi = vertices[p_i][0];
+			let nextPi = vertices[p_i][1];
+			let prevVCell = fpvd.getCellFromSite(prevPi);
 		}
 	}
 }
