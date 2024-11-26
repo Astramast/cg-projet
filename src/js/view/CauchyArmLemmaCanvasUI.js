51c5ca8b30e1aa5145dfc719e406350a6f6170c7
hcae0001 <hamzacaeyman@ulb.be>
2024-11-26 21:23:10 +0100

    copilot is not good

--- src/js/view/CauchyArmLemmaCanvasUI.js
+++ src/js/view/CauchyArmLemmaCanvasUI.js
@@ -3,6 +3,7 @@ class CauchyArmLemmaCanvasUI extends CanvasUI {
 		super(p);
 		this.points = [];
 		this.nbAngleToStretchangle = 1;
+		this.polygonStretched = null;
 	}
 
 	setup() {
@@ -13,6 +14,7 @@ class CauchyArmLemmaCanvasUI extends CanvasUI {
 
 	resetPoints() {
 		this.points = [];
+		this.polygonStretched = null;
 	}
 
 	draw() {
@@ -24,16 +26,27 @@ class CauchyArmLemmaCanvasUI extends CanvasUI {
 		this.p.fill("#CB9DF0");
 		this.p.rect(0, 80, this.p.width, this.p.height);
 
-
+		// Draw points
 		this.p.stroke("#FFF9BF");
 		this.p.fill("#FFF9BF");
 		for (let p of this.points) {
 			this.p.ellipse(p.x, p.y, 6, 6);
 		}
 
+		if (this.polygonStretched != null) {
+			this.p.stroke("#FFF9BF");
+			this.p.fill("#ba76f1");
+			this.p.beginShape();
+			for (let p of this.polygonStretched) {
+				this.p.vertex(p.x, p.y);
+			}
+			this.p.endShape(this.p.CLOSE);
+		}
+
 		// drawing lines
+		this.p.stroke("#8E24AA");
+		this.p.fill("#ba76f1");
 		this.p.beginShape();
-		this.p.noFill();
 		for (let p of this.points) {
 			this.p.vertex(p.x, p.y);
 		}
@@ -47,29 +60,41 @@ class CauchyArmLemmaCanvasUI extends CanvasUI {
 		// console.log(this.points);
 	}
 
-	Stretch() {
-		this.chooseStretch(this.points);
-		// this.test();
-	}
-
-	test() {
-		this.testGetAngle();
-		this.testRotate();
-	}
-
 
 	isConvex(polygon) {
 		let hull = computeConvexHull(polygon);
-		return polygon.length === hull.length;
+		let is_convex = (polygon.length == hull.length);
+
+		if (!is_convex) {
+			console.log("ERROR : polygon not convex, hull length :", hull.length, "polygon length :", polygon.length);
+		} else {
+			console.log("isConvex success")
+		}
+		return is_convex;
 	}
 
-	getAngle(a, b, c) {  // made by copilot
+	testIsConvex() {
+		let a = new Point(0, 0);
+		let b = new Point(1, 0);
+		let c = new Point(1, 1);
+		let d = new Point(0, 1);
+		let polygon = [a, b, c];
+		let result = this.isConvex(polygon);
+		if (!result) {
+			console.log("ERROR : isConvex failed, result :", result)
+		} else {
+			console.log("isConvex success")
+		}
+	}
+
+	getAngle(a, b, c) {  // shit by copilot
 		let v1 = [b.x - a.x, b.y - a.y];
 		let v2 = [c.x - b.x, c.y - b.y];
 		let dot = v1[0] * v2[0] + v1[1] * v2[1];
 		let det = v1[0] * v2[1] - v1[1] * v2[0];
 		let angle = Math.atan2(det, dot) * 180 / Math.PI;
-		return angle < 0 ? angle + 360 : angle;
+		angle = angle < 0 ? angle + 360 : angle;
+		return angle > 180 ? 360 - angle : angle;
 	}
 
 	testGetAngle() {
@@ -84,7 +109,8 @@ class CauchyArmLemmaCanvasUI extends CanvasUI {
 		}
 	}
 
-	rotate(origin, p, angle) {  // made by copilot
+	rotate(origin, p, angle) {  // shit by copilot
+		// need to choose clockwise or couterclkws rotation
 		let radians = angle * Math.PI / 180;
 		let cos = Math.cos(radians);
 		let sin = Math.sin(radians);
@@ -108,46 +134,73 @@ class CauchyArmLemmaCanvasUI extends CanvasUI {
 
 	propagateStretch(c, polygon, angle) {
 		let n = polygon.length;
-		for (let p = c; p !== n-1; p++) {
-			let p0 = p - 1;
+		let p = c;
+		console.log("propagateStretch", c, p, angle);
+		while (p < n && p != 0) {
+			let p0 = (p - 1 + n) % n;
 			polygon[p] = this.rotate(polygon[p0], polygon[p], angle);
+			console.log("propagateStretch", c, p, angle);
+			p = (p + 1) % n;
 		}
 	}
 
 	stretchPolygon(polygon, a) {
 		// stretch polygon at angle a-b-c and propagate the stretch
+		// a is an index
 		if (!this.isConvex(polygon)) {
-			console.log("ERROR : polygon not convex, no stretching possible")
+			console.log("ERROR : isConvex failed");
 			return;
 		}
 
 		let n = polygon.length;
 		let b = (a + 1) % n;
 		let c = (b + 1) % n;
-		while (c !== 0) {
-			let phy = this.getAngle(polygon[a], polygon[b], polygon[c]);
-			let diff = 180 - phy;
+		while (c != 0) {
+			
+			let theta = this.getAngle(polygon[a], polygon[b], polygon[c]);
+			let diff = 180 - theta;
 			if (diff > 0) {
-				let new_phy = phy + (diff / 2);
-				this.propagateStretch(a, b, c, polygon, new_phy);
+				let new_theta = (theta + (diff / 2)) % 180;
+				console.log("stretchPolygon", a, b, c, polygon, theta, diff, new_theta);
+				this.propagateStretch(c, polygon, new_theta);
+				console.log("new angle abc :", this.getAngle(polygon[a], polygon[b], polygon[c]));
 			} else {
-				console.log("ERROR : polygon not convex, no stretching possible")
+				console.log("ERROR : polygon not convex, angle diff null or negative :", diff, theta, a, b, c)
 				break;
 			}
+
+			b = (b + 1) % n;
+			c = (c + 1) % n;
+			
 		}
 	}
 
 	chooseStretch(polygon) {
 		// the user choose a nb x and we choose x angle to stretch
 		let n = polygon.length;
-		let angleToStretch = [polygon[1]];
-		// let angleToStretch = [];
-		// for (let i = 0; i < this.nbAngleToStretch; i++) {  // we push random values between 0 and n-2, n and n-1 are exluded
-		// 	angleToStretch.push(Math.floor(Math.random() * (n - 1)));  // made by copilot
-		// }
+		// let angleToStretch = [0];
+		let angleToStretch = [];
+		for (let i = 0; i < this.nbAngleToStretch; i++) {  // we push random values between 0 and n-2, n and n-1 are exluded
+			angleToStretch.push(Math.floor(Math.random() * (n - 1)));  // made by copilot
+		}
 
 		for (let a = 0; a < this.nbAngleToStretch; a++) {
 			this.stretchPolygon(polygon, angleToStretch[a]);
 		}
 	}
+
+	test() {
+		this.testGetAngle();
+		this.testRotate();
+		this.testIsConvex();
+	}
+
+	Stretch() {
+		// this.test();
+		// this.chooseStretch(this.points);
+		this.polygonStretched = [...this.points]
+		this.stretchPolygon(this.polygonStretched, 0)
+		// this.isConvex(this.points);
+	}
+
 }
